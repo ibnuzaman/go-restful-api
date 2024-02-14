84a1b7fb9d614380e925ce85af36adc45f388d0b
ibnuzaman <ibnuzaman2133@gmail.com>
2024-02-14 17:44:58 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"go-restful-api/app"
+	"go-restful-api/controller"
+	"go-restful-api/helper"
+	"go-restful-api/middleware"
+	"go-restful-api/repository"
+	"go-restful-api/service"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: middleware.NewAuthMiddleware(router),
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
